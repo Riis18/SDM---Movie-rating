a17aa74bce9c9d1acd666aa9a8e11442b98a8f70
Oliver Sylvest Hansen <oliv9858@easv365.dk>
2018-10-30 12:48:55 +0100

    First test

--- MovieRating/MovieRatingTest/MovieRatingServiceTest.cs
+++ MovieRating/MovieRatingTest/MovieRatingServiceTest.cs
@@ -1,3 +1,4 @@
+using MovieRating.Infrastructure.Data;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace MovieRatingTest
 {
     public class MovieRatingServiceTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private MovieRatingRepository movieRR = new MovieRatingRepository(new JsonRead("TestRatings.json"));
 
+        //1
+        [Theory]
+        [InlineData(1, 5)]
+        [InlineData(2, 5)]
+        [InlineData(3, 5)]
+        [InlineData(4, 5)]
+        public void NumberReviewsFromReviewer(int input, int result)
+        {
+            int reviewsFromReviewer = movieRR.GetReviewsFromReviewer(input);
+            Assert.Equal(result, reviewsFromReviewer);
         }
     }
 }
