d22dcd60d2cd4ba2b4d54c5ba7388bb33489970e
Oliver Sylvest Hansen <oliv9858@easv365.dk>
2018-10-30 09:49:06 +0100

    Added Entity

--- MovieRating/MovieRating.Core.Entities/MovieRating.cs
+++ MovieRating/MovieRating.Core.Entities/MovieRating.cs
@@ -4,5 +4,14 @@ namespace MovieRating.Core.Entities
 {
     public class MovieRating
     {
+        public int Reviewer { get; set; }
+        public int Movie { get; set; }
+        public int Grade { get; set; }
+        public DateTime Date { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Reviewer: {0}\nMovie: {1}\nGrade: {2}\nDate: {3}\n", Reviewer, Movie, Grade, Date);
+        }
     }
 }
